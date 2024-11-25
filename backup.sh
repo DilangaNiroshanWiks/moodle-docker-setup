03d9ecf77200ff6c879c419796c446d0414aa159
DilangaNiroshanWiks <2021is104@stu.ucsc.cmb.ac.lk>
2024-11-26 01:05:37 +0530

    Update backup script and database backup

--- backup.sh
+++ backup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # Variables
-CONTAINER_NAME="moodle_mariadb_1"  # Name of your MariaDB container
+CONTAINER_NAME="moodle-docker-setup-mariadb-1"
 BACKUP_FILE="moodle_backup.sql"
 
-# Backup the database using mariadb-dump
-docker exec -i "$CONTAINER_NAME" /opt/bitnami/mariadb/bin/mariadb-dump -u bn_moodle -p bitnami_moodle > "$BACKUP_FILE"
+# Use docker-compose exec instead of docker exec
+docker compose exec -T mariadb /opt/bitnami/mariadb/bin/mariadb-dump -u bn_moodle -p bitnami_moodle > "$BACKUP_FILE"
 
 # Inform the user
 echo "Backup of Moodle database created: $BACKUP_FILE"
